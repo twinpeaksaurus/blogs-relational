ec8685940fe703f9bd133cb6705cc2e1cd843e76
Joe Mogavero <jmogavero2@gmail.com>
2022-12-29 16:29:30 -0600

    added PUT for likes to change

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -22,16 +22,31 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.delete('/api/blogs/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
         const blog = await Blog.findByPk(req.params.id);
         if (blog) {
             console.log(blog);
             blog.destroy();
+            return res.status(200);
         }
     } catch (e) {
-        return res.status(400).json(e);
+        return res.status(204).json(e);
     }
 });
+
+router.put('/:id', async (req, res) => {
+    try {
+        const blog = await Blog.findByPk(req.params.id);
+        if (blog) {
+            blog.likes = req.body.likes;
+            blog.save();
+            return res.json(blog);
+        }
+    }
+    catch (e) {
+        return res.status(400).json(e);
+    }
+})
 
 module.exports = router
