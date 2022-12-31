4d4f7777b3999f502705b8ff929493a41c911ab7
Joe Mogavero <jmogavero2@gmail.com>
2022-12-31 17:21:04 -0600

    creation of blog now contains userId of user who creates

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,11 +1,33 @@
 const router = require('express').Router();
-const { Blog } = require('../models');
+const { Blog, User } = require('../models');
+const { SECRET } = require('../util/config');
+const jwt = require('jsonwebtoken')
 
-//middleware to identify blog
-const blogFinder = async (req, res, next) => {
-    req.blog = await Blog.findByPk(req.params.id);
+
+
+//middleware to extract token from request
+const tokenExtractor = (req, res, next) => {
+    //what does req.get do below???
+    const authorization = req.get('authorization');
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        try {
+            req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+        } catch {
+            return res.status(401).json({ error: 'token invalid' });
+        }
+    } else {
+        console.log(authorization);
+
+        return res.status(401).json({ error: 'token missing' });
+    }
     next();
 }
+
+//middleware to identify blog
+// const blogFinder = async (req, res, next) => {
+//     req.blog = await Blog.findByPk(req.params.id);
+//     next();
+// }
 
 
 router.get('/', async (req, res) => {
@@ -13,9 +35,12 @@ router.get('/', async (req, res) => {
     res.json(blogs);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', tokenExtractor, async (req, res) => {
     try {
-        const blog = await Blog.create(req.body);
+        const user = await User.findByPk(req.decodedToken.id)
+
+        //will user.name work here for authorship?
+        const blog = await Blog.create({ ...req.body, userId: user.id, author: user.name });
         return res.json(blog);
     } catch (e) {
         return res.status(400).json(e);
